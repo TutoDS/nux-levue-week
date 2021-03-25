ab28363778dcf9b319d0be6a3b0e7fd6c2d59f09
Daniel Sousa @TutoDS <jdaniel.asousa@gmail.com>
2021-03-25 08:18:49 +0000

    First classe

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+	root: true,
+	env: {
+		browser: true,
+		node: true
+	},
+	extends: ['@nuxtjs/eslint-config-typescript', 'plugin:nuxt/recommended'],
+	plugins: [],
+	// add your custom rules here
+	rules: {
+		'comma-dangle': ['error', 'never'],
+		'import/first': 'off',
+		'import/order': [
+			'error',
+			{
+				groups: [
+					'index',
+					'sibling',
+					'parent',
+					'internal',
+					'external',
+					'builtin',
+					'object'
+				]
+			}
+		],
+
+		indent: 'off',
+		'@typescript-eslint/indent': ['error', 'tab'],
+		'no-new': 'off',
+		'no-tabs': 'off',
+		semi: ['error', 'always'],
+		'vue/html-indent': ['error', 'tab'],
+		'vue/order-in-components': 'off'
+	}
+};
